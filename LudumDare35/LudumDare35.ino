af89b541b1ff449199caa2eceb9e38135e000e3e
koryb <byrnek@mymail.shawnee.edu>
2016-04-18 03:04:56 -0400

    It works with inputs!

--- LudumDare35/LudumDare35.ino
+++ LudumDare35/LudumDare35.ino
@@ -23,6 +23,38 @@ void setup()
 {                          
   pinMode(WS2812_pin, OUTPUT);
   
+  //player 1 (left player)
+
+  //red
+  pinMode(38,INPUT);
+  //yellow
+  pinMode(39,INPUT);
+
+  //down
+  pinMode(40,INPUT);
+  //right
+  pinMode(41,INPUT);
+  //up
+  pinMode(42,INPUT);
+  //left
+  pinMode(43,INPUT);
+ 
+
+  
+  // player 2 (right player)
+  //yellow
+  pinMode(46,INPUT);
+  //left
+  pinMode(47,INPUT);
+  //down
+  pinMode(48,INPUT);
+  //red
+  pinMode(49,INPUT);
+  //right
+  pinMode(50,INPUT);
+  //up
+  pinMode(51,INPUT);
+  
   Serial.begin(9600);
 
   testPiece = makeT(5, 5, testPiece);
@@ -36,10 +68,23 @@ void loop()
   elapsedTime = currentTime - lastTime - lastDelay;
   lastTime = currentTime;
 
-  if (count++ == 30)
+  if (digitalRead(49) == LOW)
   {
-    count = 0;
     rotatePieceCCW(testPiece);
+  }
+  else if (digitalRead(46) == LOW)
+  {
+    rotatePieceCW(testPiece);
+  } 
+
+  if (digitalRead(47) == LOW)
+  {
+    testPiece->x --;
+  }
+
+  if (digitalRead(50) == LOW)
+  {
+    testPiece->x ++;
   }
 
   color.longColor = 0x000000FF;
